ebeec672ccf546a67e8d50b268efbcc3db8210f1
Dhruv Kathuria <s3993592@student.rmit.edu.au>
2025-06-10 12:35:03 +1000

    edited app.py

--- Ai-chatbot-main/app.py
+++ Ai-chatbot-main/app.py
@@ -8,8 +8,8 @@ MODEL_ID = "anthropic.claude-3-haiku-20240307-v1:0"
 IDENTITY_POOL_ID = "us-east-1:7771aae7-be2c-4496-a582-615af64292cf"
 USER_POOL_ID = "us-east-1_koPKi1lPU"
 APP_CLIENT_ID = "3h7m15971bnfah362dldub1u2p"
-USERNAME=""
-PASSWORD=""
+USERNAME="s3993592@student.rmit.edu.au"
+PASSWORD="Dhruv2580@"
 
 def get_credentials(username, password):
     """Get AWS credentials"""
